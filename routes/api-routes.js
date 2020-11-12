94a1bc0de54091a154f224375e7e8e2956542472
Jeff T <jeff.thao92@gmail.com>
2020-11-11 23:56:06 -0600

    Added routes folder with api-routes.js file. Tried to do a get(/api/workouts) to call the Workout schema in the models/workout.js file.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,20 +1,15 @@
-const { model } = require("mongoose");
-// let db = require("../models");
-console.log("api routes");
-model.exports = function(app) {
-    app.get("/api/workouts", function(req, res) {
-        console.log("workouts working");
+console.log("api routes connected");
+const db = require("../models");
 
-      });
-
-      app.get("/api/workouts/:id", function(req, res) {
-        console.log("workouts id working");
-        
-      });
-
-      app.get("/api/workouts/range", function(req, res) {
-        console.log("workouts range working");
-
-      });
+module.exports = function (app) {
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+    .then(workout => {
+      res.json(workout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  });
 
 }
