d4bbb235657f31854cf48ab796edfcc3048cec44
Jeff T <jeff.thao92@gmail.com>
2020-11-12 21:10:21 -0600

    Added a getDuration function that copies the tallyExercises function. getDuration will add all the durations of every exercise.

--- public/workout.js
+++ public/workout.js
@@ -8,7 +8,7 @@ async function initWorkout() {
 
     const workoutSummary = {
       date: formatDate(lastWorkout.day),
-      totalDuration: lastWorkout.totalDuration,
+      ...getDuration(lastWorkout.exercises),
       numExercises: lastWorkout.exercises.length,
       ...tallyExercises(lastWorkout.exercises)
     };
@@ -17,6 +17,15 @@ async function initWorkout() {
   } else {
     renderNoWorkoutText()
   }
+}
+
+function getDuration(exercises) {
+  const tallied = exercises.reduce((acc, curr) => {
+
+      acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
+    return acc;
+  }, {});
+  return tallied;
 }
 
 function tallyExercises(exercises) {
