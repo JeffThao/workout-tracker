3cccb1d2ff53c1939f0cd3572b6ccd8f490eae99
Jeff T <jeff.thao92@gmail.com>
2020-11-12 20:57:21 -0600

    added router.get('/api/workouts') Workout.find({}) to find the last workout so it shows up on the main page.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,5 +12,16 @@ router.get("/api/workouts/range", (req, res) => {
       res.status(400).json(err);
     });
 });
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
 
 module.exports = router;
